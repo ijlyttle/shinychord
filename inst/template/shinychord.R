9a29ec302ebb50655e0fc5b219b67aa79bf97d94
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2015-08-20 18:15:22 -0500

    template file

---
+++ inst/template/shinychord.R
@@ -0,0 +1,51 @@
+#' template
+#'
+#' Creates a list of objects that can be placed in a shiny application
+#'
+#' The list will contain:
+#'
+#' \itemize{
+#'   \item \code{ui_controller} \code{shiny::taglist} of ui elements for the controller
+#'   \item \code{ui_view} \code{shing::taglist} of ui elements for the view
+#'   \item \code{server_model} a function with reactive code - this function takes a \code{reactiveValues} as its arguement
+#' }
+#'
+#' The list returned by the factory has to be available to both the ui and the server. If not using the \code{shinyApp}
+#' formulation, perhaps \code{global.R} could be useful.
+#'
+#' The component \code{server_model} will be called in the server function, using the
+#' particular \code{reactiveValue} you wish to associate with the "thing".
+#'
+#' @param id    character, tag to prepend to the input and output id's
+#'
+#' @return list containing \code{ui_controller}, \code{ui_view}, and \code{srv_model}
+#' @export
+#'
+template <- function(id){
+
+  id_name <- function(...){
+    paste(list(id, ...), collapse = "_")
+  }
+
+  ## ui_controller ##
+  ui_controller <- shiny::tagList()
+
+
+  ## ui_view ##
+  ui_view <- shiny::tagList()
+
+
+  ## server_model ##
+  server_model <- function(){
+
+    env = parent.frame()
+
+  }
+
+  list(
+    ui_controller = ui_controller,
+    ui_view = ui_view,
+    server_model = server_model
+  )
+
+}
