2f1585417f9b01aef6fbf9542dd92f40fbcb7994
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2015-09-07 20:25:25 -0500

    updates

--- R/tbldf_read_delim.R
+++ R/tbldf_read_delim.R
@@ -1,46 +1,3 @@
-#' tbldf_read
-#'
-#' Given some text, a delimiter, and tz attributes, return a data-frame
-#'
-#' @param file Passed to \code{readr::read_delim}. Either a path to a file, a connection, or literal data (either a single string or a raw vector).
-#' @param delim Passed to \code{readr::read_delim}. Delimiter.
-#' @param tz_file Timezone used to express the time in the file, see \code{lubridate::olson_time_zones()}
-#' @param tz_location Timezone used to express the time at the location, see \code{lubridate::olson_time_zones()}
-#'
-#' @return \code{dplyr::tbl_df()} Dataframe-like structure
-#' @export
-#'
-tbldf_read <- function(file, delim = ",", tz_file = "UTC", tz_location = "UTC"){
-
-  # make a provisional parsing of the data-frame
-  df <- readr::read_delim(file = file, delim = delim)
-
-  # determine which columns are datetimes
-  names_posixct <- df_names_inherits(df, "POSIXct")
-  list_posixct <- as.list(names_posixct)
-  names(list_posixct) <- names_posixct
-
-  # run this only if there are datetime columns
-  if (length(names_posixct) > 0){
-
-    fn_parse <- function(x) {
-      readr::col_datetime(tz = tz_file)
-    }
-
-    # set datetime parsers for the datetime columns
-    list_parse_datetime <- lapply(list_posixct, fn_parse)
-
-    # re-parse, using these parsers
-    df <- readr::read_delim(file = file, delim = delim, col_types = list_parse_datetime)
-
-    # set the timezone
-    df <- df_set_tz(df, tz_location)
-
-  }
-
-  df
-}
-
 #' tbldf_read_delim
 #'
 #' Creates a collection of shiny objects to manage the parsing of a delimited file.
@@ -155,7 +112,7 @@ tbldf_read_delim <- function(id){
     # sets the reactive output
     observe({
       rctval[[item]] <-
-        tbldf_read(
+        xformplus::tbldf_read(
           file = rct_txt(),
           delim = env$input[[id_controller_delim]],
           tz_file = env$input[[id_controller_tzfile]],
