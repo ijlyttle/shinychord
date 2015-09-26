8f70cb6418894d4e3d8794dfe8df29e69117355e
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2015-09-25 19:52:02 -0500

    removes rctval_names

--- R/utils.R
+++ R/utils.R
@@ -1,23 +1,7 @@
-#' rctval_names
-#'
-#' @param rctval
-#'
-#' @return character vector
-#' @export
-#'
-rctval_names <- function(rctval){
 
-  lst <- shiny::reactiveValuesToList(rctval)
 
-  is_null <- vapply(lst, is.null, FALSE)
 
-  names(lst[!is_null])
-
-}
-
-#' df_names_inherits
-#'
-#' get the names of all the columns of the dataframe
+#' Get the names of all the columns of the dataframe
 #' that inherit from the supplied class name
 #'
 #' @param df     dataframe
