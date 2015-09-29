f6dc34196bc7fe7b0b99add1d4cae424e15b0590
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2015-09-29 14:21:24 -0500

    adds wx_ames dataset

---
+++ R/wx_ames.R
@@ -0,0 +1,29 @@
+#' Weather observations at Ames - Jan. 2014
+#'
+#' A dataset containing hourly weather observations at Ames, Iowa (US) for
+#' the month of January 2014. Data obtained from Weather Undergound's API.
+#'
+#' @format A data frame with 983 rows and 19 variables:
+#' \describe{
+#'   \item{date}{}
+#'   \item{temp}{}
+#'   \item{dew_pt}{}
+#'   \item{hum}{}
+#'   \item{wind_spd}{}
+#'   \item{wind_gust}{}
+#'   \item{dir}{}
+#'   \item{vis}{}
+#'   \item{pressure}{}
+#'   \item{wind_chill}{}
+#'   \item{heat_index}{}
+#'   \item{precip}{}
+#'   \item{cond}{}
+#'   \item{fog}{}
+#'   \item{rain}{}
+#'   \item{snow}{}
+#'   \item{hail}{}
+#'   \item{thunder}{}
+#'   \item{tornado}{}
+#' }
+#' @source \url{http://www.wunderground.com/}
+"wx_ames"
